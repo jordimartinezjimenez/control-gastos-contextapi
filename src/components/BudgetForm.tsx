e60623690d6e83ba29a523bc635f4099045b847c
unknown <jordimj63@gmail.com>
2024-07-28 17:27:49 +0200

    Budget form + its validation and the Reducer

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, useMemo, useState } from "react"
+
+export default function BudgetForm() {
+
+    const [budget, setBudget] = useState(0)
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setBudget(+e.target.value)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="textl-4xl text-blue-600 font-bold text-center">Definir presupuesto</label>
+                <input
+                    type="number"
+                    className="w-full bg-white border border-gray-200 p-2 text-center bg-slate-100"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    id=" budget"
+                    value={budget}
+                    onChange={handleChange}
+                />
+            </div>
+            <input
+                type="submit"
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40"
+                value="Definir Presupuesto"
+                disabled={isValid}
+            />
+        </form>
+    )
+}
