ab230fdc653fb3432dde9a0d1d3b50eede073864
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-08-31 16:21:21 +0200

    Added context and reducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useContext } from "react"
+import { useMemo } from "react"
 import BudgetForm from "./components/BudgetForm"
-import { BudgetContext } from "./components/context/BudgetContext"
+import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from "./components/BudgetTracker"
 
 function App() {
 
-  const context = useContext(BudgetContext)
-  console.log(context)
+  const { state } = useBudget()
+
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
 
   return (
     <>
@@ -14,7 +16,7 @@ function App() {
       </header>
 
       <div className="max-w-3xl mx-auto bg-white shadow-lg  rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
     </>
   )
