18fe2902e7137a90c93f04c70a61690aa7727008
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-08-31 16:36:44 +0200

    Budget tracker component added (interface only, temporarily non-functional)

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg  rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+
+      <div>
+        
+      </div>
     </>
   )
 }
