8c21181c3e24586628ab4c295476570d9ba3d552
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-09-03 21:40:37 +0200

    Expense List added

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseModal from "./components/ExpenseModal"
+import ExpenseList from "./components/ExpenseList"
 
 function App() {
 
@@ -22,7 +23,8 @@ function App() {
 
       {isValidBudget && (
         <main className="max-w-3xl mx-auto py-10">
-          < ExpenseModal />
+          <ExpenseList />
+          <ExpenseModal />
         </main>
       )}
     </>
