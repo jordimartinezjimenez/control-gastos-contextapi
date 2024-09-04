2e51f67d823713c8f8fbca4dbb2fe46be0e743cf
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-09-04 21:41:34 +0200

    Filter for expense categories

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -43,7 +43,7 @@ export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
                 leadingActions={leadingActions()}
                 trailingActions={trailingActions()}
             >
-                <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center select-none">
+                <div className="bg-white shadow-lg p-5 w-full border-b border-gray-200 flex gap-5 items-center select-none">
                     <div>
                         <img
                             src={`/icono_${categoryInfo.icon}.svg`}
